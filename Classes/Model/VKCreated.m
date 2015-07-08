b49552b4f88129e021fae45f4158b9c7e80b692b
Christopher Luu <nuudles@gmail.com>
2015-07-07 22:04:27 -0400

    Update date format to reflect latest version of the API

--- Classes/Model/VKCreated.m
+++ Classes/Model/VKCreated.m
@@ -39,7 +39,7 @@
 
 + (NSValueTransformer *)creationDateJSONTransformer {
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSS"];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSZ"];
     
     return [MTLValueTransformer transformerUsingForwardBlock:^id(NSString *dateString, BOOL *success, NSError *__autoreleasing *error) {
     
