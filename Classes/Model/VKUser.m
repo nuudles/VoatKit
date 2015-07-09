800b6871e7a3f40a9c9877b4e02dee4cb43addd8
Christopher Luu <nuudles@gmail.com>
2015-07-09 17:34:27 -0400

    Updated VKUser object with more properties Added processing for registrationDate, commentPoints, submissionPoints, commentVoting, and submissionVoting

--- Classes/Model/VKUser.m
+++ Classes/Model/VKUser.m
@@ -18,9 +18,27 @@
              @"username": @"userName",
              @"bio": @"bio",
              @"profilePicture": @"profilePicture",
-             @"badges": @"badges"
+             @"badges": @"badges",
+             @"registrationDate": @"registrationDate",
+             @"commentPoints": @"commentPoints",
+             @"submissionPoints": @"submissionPoints",
+             @"commentVoting": @"commentVoting",
+             @"submissionVoting": @"submissionVoting"
              };
 }
 
+
+#pragma mark - MTLModel
+
++ (NSValueTransformer *)registrationDateJSONTransformer {
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSZ"];
+
+    return [MTLValueTransformer transformerUsingForwardBlock:^id(NSString *dateString, BOOL *success, NSError *__autoreleasing *error) {
+        return [dateFormatter dateFromString:dateString];
+    }];
+
+    return nil;
+}
 
 @end
