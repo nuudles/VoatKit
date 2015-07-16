805a3ae716589180fd3290c72f38f2c65787f0fd
Amar Ramachandran <amarrmchndrn1@gmail.com>
2015-07-16 12:55:58 -0700

    Removed warning & fixed search

--- Classes/Model/VKSearchOptions.m
+++ Classes/Model/VKSearchOptions.m
@@ -1,80 +1,106 @@
 //
-//  VKSearchOptions.h
+//  NSObject+VKSearchOptions.m
 //  VoatKit
 //
 //  Created by Amar Ramachandran on 6/27/15.
 //  Copyright © 2015 AmarJayR. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
-@interface VKSearchOptions : NSObject
-
-typedef NS_ENUM(NSUInteger, VKUserContentSortingMethod) {
-    VKUserContentSortingMethodNew = 1,
-    VKUserContentSortingMethodTop,
-    VKUserContentSortingMethodHot
-};
-
-typedef NS_ENUM(NSUInteger, VKSortSpanMethod) {
-    VKTimeSortingMethodAllTime = 1,
-    VKTimeSortingMethodThisHour,
-    VKTimeSortingMethodToday,
-    VKTimeSortingMethodThisWeek,
-    VKTimeSortingMethodThisMonth,
-    VKTimeSortingMethodThisQuarter,
-    VKTimeSortingMethodThisYear,
-};
-
-extern NSString * VKStringFromSortSpanMethod(VKSortSpanMethod sortingMethod);
-extern NSString * VKStringFromUserContentSortingMethod(VKUserContentSortingMethod sortingMethod);
-
-/**
- The total number of things to return. This is 25 by default, and limited to 50.
- */
-@property (nonatomic, copy) NSNumber* count;
+#import "VKSearchOptions.h"
+#import <objc/runtime.h>
 
 
-/**
- The sorting method for user content. This affects the order in which user content is returned.
- 
- */
-@property (nonatomic, assign) VKUserContentSortingMethod sort;
+NSString * VKStringFromTimeSortingMethod(VKSortSpanMethod sortingMethod)
+{
+    switch (sortingMethod)
+    {
+        case VKTimeSortingMethodThisHour:
+            return @"hour";
+        case VKTimeSortingMethodToday:
+            return @"day";
+        case VKTimeSortingMethodThisWeek:
+            return @"week";
+        case VKTimeSortingMethodThisMonth:
+            return @"month";
+        case VKTimeSortingMethodThisYear:
+            return @"year";
+        case VKTimeSortingMethodAllTime:
+            return @"all";
+        default:
+            return nil;
+    }
+}
 
-/**
- The timeframe to sort by.
- */
-@property (nonatomic, assign) VKSortSpanMethod span;
-
-/**
- The index to start from
- */
-@property (nonatomic, copy) NSNumber* index;
-
-/**
- The page to start from
- @note: setting this will override the index property
- */
-@property (nonatomic, copy) NSNumber* page;
-
-/**
- The search value to match for submissions or comments
- */
-@property (nonatomic, copy) NSString* search;
-
-/**
- Specifies the depth of comment tree to retrieve. Used only for comment queries.
- */
-@property (nonatomic, copy) NSNumber* depth;
+NSString * VKStringFromUserContentSortingMethod(VKUserContentSortingMethod sortingMethod)
+{
+    switch (sortingMethod)
+    {
+        case VKUserContentSortingMethodNew:
+            return @"new";
+        case VKUserContentSortingMethodHot:
+            return @"hot";
+        case VKUserContentSortingMethodTop:
+            return @"top";
+        default:
+            return nil;
+    }
+}
 
 
-/**
- *  Turns the object into a query dictionary.
- *
- *  @return NSDictionary object to append to the end of the URL.
- */
--(NSDictionary *)dictionaryValue;
+@implementation VKSearchOptions
 
--(void)searchOptionsFromQueryString:(NSString*) queryString;
+- (void)setCount:(NSNumber*)count
+{
+    _count = @(MIN(50, [count intValue]));
+}
+
+-(NSDictionary *)dictionaryValue {
+    NSMutableDictionary* keyValuePairs = [[NSMutableDictionary alloc] init];
+    
+    if(self.span)
+        [keyValuePairs setObject:@(self.span-1) forKey:@"span"];
+    if(self.sort)
+        [keyValuePairs setObject:@(self.sort-1) forKey:@"sort"];
+    if(self.count)
+        [keyValuePairs setObject:self.count forKey:@"count"];
+    if(self.index)
+        [keyValuePairs setObject:self.index forKey:@"index"];
+    if(self.page)
+        [keyValuePairs setObject:self.page forKey:@"page"];
+    if(self.search)
+        [keyValuePairs setObject:self.search forKey:@"search"];
+    if(self.depth != NULL)
+        [keyValuePairs setObject:self.depth forKey:@"depth"];
+    
+    return keyValuePairs;
+}
+
+
+-(void)searchOptionsFromQueryString:(NSString*) queryString {
+    /*//span=1&sort=2
+    NSArray* keyValuePairs = [queryString componentsSeparatedByString:@"&"];
+    NSMutableDictionary* dictionaryPairs = [[NSMutableDictionary alloc] initWithCapacity:[keyValuePairs count]];
+    
+    for (NSString* keyValue in keyValuePairs) {
+        NSString* key = [keyValue componentsSeparatedByString:@"="][0];
+        if (![self respondsToSelector:NSSelectorFromString(key)]) {
+            continue;
+        }
+        
+        NSString* value = [keyValue componentsSeparatedByString:@"="][1];
+        
+        NSLog(@"CLASS: %@", [[self valueForKey:key] class]);
+        if ([[[self valueForKey:key] class] isSubclassOfClass:[NSNumber class]]) {
+            NSLog(@"IS SUBCLASS: %@", key);
+            [self setValue:[NSNumber numberWithInteger:[value integerValue]] forKey:key];
+            continue;
+        }
+        
+        [self setValue:value forKey:key];
+        
+    }*/
+}
+
+
 
 @end
