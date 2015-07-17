10e6bb40cf994dfc389797f3b2fa35023d3419f4
Amar Ramachandran <amarrmchndrn1@gmail.com>
2015-07-17 15:57:55 -0700

    Fixed Comments

--- Classes/Networking/VKClient+Comments.m
+++ Classes/Networking/VKClient+Comments.m
@@ -14,29 +14,55 @@
 #pragma mark - Getting Comments
 
 - (NSURLSessionDataTask *)commentsForSubmission:(VKSubmission *)submission searchOptions:(VKSearchOptions*)searchOptions completion:(VKArrayCompletionBlock)completion {
+    NSParameterAssert(submission);
+    
     return [self commentsForSubmissionWithIdentifier:submission.submissionID withSubverse:submission.subverse searchOptions:searchOptions completion:completion];
 }
 
 - (NSURLSessionDataTask *)commentsForSubmissionWithIdentifier:(NSNumber *)submissionIdentifier withSubverse:(NSString*)subverse searchOptions:(VKSearchOptions*)searchOptions completion:(VKArrayCompletionBlock)completion {
+    NSParameterAssert(submissionIdentifier);
+    NSParameterAssert(subverse);
+    
     return [self arrayTaskWithPath:[NSString stringWithFormat:@"api/v1/v/%@/%@/comments", subverse, submissionIdentifier] parameters:nil searchOptions:searchOptions completion:completion];
 }
 
 #pragma mark - Submitting Comments
 
 - (NSURLSessionDataTask *)submitComment:(NSString *)commentText onSubmission:(VKSubmission *)submission completion:(VKObjectCompletionBlock)completion {
+    NSParameterAssert(commentText);
+    NSParameterAssert(submission);
+    
     return [self submitComment:commentText onSubmissionWithIdentifier:submission.submissionID withSubverse:submission.subverse completion:completion];
 }
 
 - (NSURLSessionDataTask *)submitComment:(NSString *)commentText onSubmissionWithIdentifier:(NSNumber *)submissionIdentifier withSubverse:(NSString*)subverse completion:(VKObjectCompletionBlock)completion {
-    return [self postWithResponse:[NSString stringWithFormat:@"api/v1/v/%@/%@/comment", subverse, submissionIdentifier] parameters:nil completion:completion];
+    NSParameterAssert(commentText);
+    NSParameterAssert(submissionIdentifier);
+    NSParameterAssert(subverse);
+    
+    NSMutableDictionary *parameters = [[NSMutableDictionary alloc] initWithCapacity:2];
+    [parameters setObject:commentText forKey:@"value"];
+    
+    return [self postWithResponse:[NSString stringWithFormat:@"api/v1/v/%@/%@/comment", subverse, submissionIdentifier] parameters:parameters completion:completion];
 }
 
 - (NSURLSessionDataTask *)submitComment:(NSString *)commentText asReplyToComment:(VKComment *)comment completion:(VKObjectCompletionBlock)completion {
+    NSParameterAssert(commentText);
+    NSParameterAssert(commentText);
+    
     return [self submitComment:commentText asReplyToComment:comment.commentID withSubmissionIdentifier:comment.submissionID withSubverse:comment.subverse completion:completion];
 }
 
 - (NSURLSessionDataTask *)submitComment:(NSString *)commentText asReplyToComment:(NSNumber *)commentID withSubmissionIdentifier:(NSNumber*)submissionIdentifier withSubverse:(NSString*)subverse completion:(VKObjectCompletionBlock)completion {
-    return [self postWithResponse:[NSString stringWithFormat:@"api/v1/v/%@/%@/comment/%@", subverse, submissionIdentifier, commentID] parameters:nil completion:completion];
+    NSParameterAssert(commentText);
+    NSParameterAssert(commentID);
+    NSParameterAssert(submissionIdentifier);
+    NSParameterAssert(subverse);
+    
+    NSMutableDictionary *parameters = [[NSMutableDictionary alloc] initWithCapacity:2];
+    [parameters setObject:commentText forKey:@"value"];
+    
+    return [self postWithResponse:[NSString stringWithFormat:@"api/v1/v/%@/%@/comment/%@", subverse, submissionIdentifier, commentID] parameters:parameters completion:completion];
 }
 
 @end
